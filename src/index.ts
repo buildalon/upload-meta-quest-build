924698825d0061bd116019b3a37ae5b5eba8ab11
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-03-25 08:06:28 -0400

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -168,7 +168,7 @@ async function findSpecificPath(pattern: string): Promise<string | null> {
     } else if (paths.length > 1) {
         core.warning(`Found more than one path matching pattern: ${pattern}\n  > ${paths.join(`\n  > `)}`);
     }
-    const result = paths[0] !== null ? paths[0] : null;
+    const result = paths[0];
     core.debug(`Found path: ${result}`);
     return result;
 }
